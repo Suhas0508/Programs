3041881ff1dcdf4f8f09eabd8f70a43ec5a2a5cf
Suhas0508 <96803394+Suhas0508@users.noreply.github.com>
2021-12-30 20:15:27 +0530

    Create single_loop_pattern.py

---
+++ single_loop_pattern.py
@@ -0,0 +1,17 @@
+# Using only one loop
+# Print Tringle * Pattern
+
+# Example :
+
+# *
+# **
+# ***
+# **** 
+# *****
+
+num = int(input("Enter a number : "))
+
+i=0
+while i<=num:
+    print(i * "*")
+    i+=1
